81d8cd4672a7ccec64a5b08003389084e73a007e
rajat-dh <rajat.gangwar@foodpanda.com>
2022-01-09 17:27:53 +0530

    init commit

---
+++ app/src/main/java/com/example/myapplication/HelloWorld.kt
@@ -0,0 +1,47 @@
+package com.example.myapplication
+
+import kotlinx.coroutines.*
+
+fun main() {
+
+//    runBlocking {
+//        launch {
+//            delay(1500)
+//            println("runBlocking Execution")
+//        }
+//        GlobalScope.launch {
+//            delay(1000)
+//            println("GlobalScope Execution")
+//        }
+//        val job = coroutineScope {
+//            delay(500)
+//            println("coroutineScope execution")
+//        }
+//    }
+//
+//    print("Hello, World!")
+//    Thread.sleep(2000)
+
+//    runBlocking {
+//        launch(Dispatchers.IO) {
+//            println("IO Thread ${Thread.currentThread().name}")
+//        }
+//
+//        launch (Dispatchers.Default) {
+//            println("Default Thread ${Thread.currentThread().name}")
+//        }
+//
+//        launch(Dispatchers.Unconfined) {
+//            println("Unconfined Thread ${Thread.currentThread().name}")
+//        }
+//
+//        launch(newSingleThreadContext("MyThread")) {
+//            println("newSingleThreadContext Thread ${Thread.currentThread().name}")
+//        }
+//    }
+
+    //TODO aync
+
+
+
+}
